48885259f3c7c6d5fa7b811095deee28beb8559b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-06-24 09:46:20 -0500

    factor out nest_app and make nesting much more robust

--- lib/Catalyst/Action/FromPlack.pm
+++ lib/Catalyst/Action/FromPlack.pm
@@ -4,13 +4,26 @@ use base 'Catalyst::Action';
 use HTTP::Message::PSGI qw(res_from_psgi);
 use Plack::App::URLMap;
 
+sub nest_app {
+   my ($self, $c, $app) = @_;
+
+   my $nest = Plack::App::URLMap->new;
+
+   my $path = '/' . $c->request->path;
+   my $rest = join '/', @{$c->request->arguments};
+   $path =~ s/\Q$rest\E$//;
+   $nest->map( $path => $app );
+
+   return $nest
+}
+
 sub execute {
    my ($self, $controller, $c, @rest) = @_;
 
-   my $app = $self->code->();
-   my $nest = Plack::App::URLMap->new;
-   $nest->map( $self->attributes->{Path}->[0] => $app);
+   my $app = $self->code->($controller, $c, @rest);
+   my $nest = $self->nest_app($c, $app);
    my $res = res_from_psgi($nest->($c->req->env));
+
    $c->res->status($res->code);
    $c->res->body($res->content);
    $c->res->headers($res->headers);
